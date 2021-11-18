f56df39e643a0949e6a98ac14d8255081ffae3ec
Keanu Interone <keanu.interone@gmail.com>
2021-11-17 17:33:28 -0800

    refactored back to using just http

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const https = require('https')
-const fs = require('fs')
 const database = require('./config/database')
 const routes = require('./config/routes')
 const auth = require('./config/auth')
@@ -20,13 +18,9 @@ auth.setUp(app);
 // SET ROUTES //
 routes.setRoutes(app)
 
-
 // APP LISTEN //
-const options = {
-	key: fs.readFileSync('./config/certs/ca.key'),
-	cert: fs.readFileSync('./config/certs/ca.cert')
-}
 const PORT = process.env.PORT
-https.createServer(options, app).listen(PORT, () => {
-	console.log("Server is running on port: " + PORT)
+const HOST = process.env.HOST
+app.listen(PORT, HOST, () => {
+    console.log("Server is running on port: " + PORT)
 })
